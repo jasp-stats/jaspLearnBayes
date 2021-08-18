f8bc4818ec6a6d24c5c636358b6d5a9034fe8796
Jiashun97 <65221219+Jiashun97@users.noreply.github.com>
2021-08-18 23:14:31 +0800

    Unit test
    add {}

--- tests/testthat/test-lsbuffonsneedlesimulation.R
+++ tests/testthat/test-lsbuffonsneedlesimulation.R
@@ -2,49 +2,51 @@ context("Learn Bayes - Buffon's Needle Simulation")
 
 
 ## default settings
+{
+  options <- analysisOptions("LSBuffonsneedlesimulation")
+  options[["n"]] <- 100
+  options[["a"]] <- 1
+  options[["b"]] <- 1
+  options[["length"]] <- 80
+  options[["CI"]] <- 0.95
+  options[["showNeedlePlot"]] <- TRUE
+  options[["showPropDistPlot"]] <- TRUE
+  options[["showPiDistPlot"]] <- TRUE
+  options[["color"]] <- TRUE
+  options[["CIArrow"]] <- TRUE
+  options[["legendPropDistPlot"]] <- TRUE
+  options[["legendPiDistPlot"]] <- TRUE
+  
+  set.seed(1)
+  dataset <- NULL
+  results <- runAnalysis("LSBuffonsneedlesimulation", dataset, options)#, makeTests = TRUE)
+  
+  
+  test_that("Needle Plot matches", {
+    plotName <- results[["results"]][["needlePlot"]][["data"]]
+    testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
+    jaspTools::expect_equal_plots(testPlot, "needle-plot")
+  })
+  
+  test_that("Implied Prior and Posterior for  π plot matches", {
+    plotName <- results[["results"]][["piDistPlot"]][["data"]]
+    testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
+    jaspTools::expect_equal_plots(testPlot, "implied-prior-and-posterior-for-π")
+  })
+  
+  test_that("Prior and Posterior for Proportion of Crosses plot matches", {
+    plotName <- results[["results"]][["propDistPlot"]][["data"]]
+    testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
+    jaspTools::expect_equal_plots(testPlot, "prior-and-posterior-for-proportion-of-crosses")
+  })
+  
+  test_that("Summary Table results match", {
+    table <- results[["results"]][["summaryTable"]][["data"]]
+    jaspTools::expect_equal_tables(table,
+                                   list(3.33, 48, 100, 2.77, 4.18))
+  })
+}
 
-options <- analysisOptions("LSBuffonsneedlesimulation")
-options[["n"]] <- 100
-options[["a"]] <- 1
-options[["b"]] <- 1
-options[["length"]] <- 80
-options[["CI"]] <- 0.95
-options[["showNeedlePlot"]] <- TRUE
-options[["showPropDistPlot"]] <- TRUE
-options[["showPiDistPlot"]] <- TRUE
-options[["color"]] <- TRUE
-options[["CIArrow"]] <- TRUE
-options[["legendPropDistPlot"]] <- TRUE
-options[["legendPiDistPlot"]] <- TRUE
-
-set.seed(1)
-dataset <- NULL
-results <- runAnalysis("LSBuffonsneedlesimulation", dataset, options)#, makeTests = TRUE)
-
-
-test_that("Needle Plot matches", {
-  plotName <- results[["results"]][["needlePlot"]][["data"]]
-  testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
-  jaspTools::expect_equal_plots(testPlot, "needle-plot")
-})
-
-test_that("Implied Prior and Posterior for  π plot matches", {
-  plotName <- results[["results"]][["piDistPlot"]][["data"]]
-  testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
-  jaspTools::expect_equal_plots(testPlot, "implied-prior-and-posterior-for-π")
-})
-
-test_that("Prior and Posterior for Proportion of Crosses plot matches", {
-  plotName <- results[["results"]][["propDistPlot"]][["data"]]
-  testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
-  jaspTools::expect_equal_plots(testPlot, "prior-and-posterior-for-proportion-of-crosses")
-})
-
-test_that("Summary Table results match", {
-  table <- results[["results"]][["summaryTable"]][["data"]]
-  jaspTools::expect_equal_tables(table,
-                                 list(3.33, 48, 100, 2.77, 4.18))
-})
 
 
 
