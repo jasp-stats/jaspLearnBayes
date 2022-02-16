5c18b75df42b933a65dc1b81650aa9882b483852
Jiashun97 <65221219+Jiashun97@users.noreply.github.com>
2022-02-16 16:49:53 +0800

    Revert "Position legends to the right"
    This reverts commit 858fe5acada61d6a7fa44ffc5cf47e8c863b8f00.

--- R/LSBuffonsneedlesimulation.R
+++ R/LSBuffonsneedlesimulation.R
@@ -216,7 +216,7 @@ LSBuffonsneedlesimulation<- function(jaspResults, dataset, options, state = NULL
 
    if (options[["legendPiDistPlot"]]){
      piDistPlot$plotObject <-  piDistPlot$plotObject + 
-       ggplot2::theme(legend.position = c(.24, .9))
+       ggplot2::theme(legend.position = "right")
    }
    
    if (options[["CIArrow"]]){
